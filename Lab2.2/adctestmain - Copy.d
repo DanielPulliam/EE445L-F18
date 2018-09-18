f03e941380eb4c7b6733a8c51801d7b4a84b8efe
DanielPulliam <43254653+DanielPulliam@users.noreply.github.com>
2018-09-18 14:06:55 -0500

    lab 2 changes

---
+++ Lab2.2/adctestmain - Copy.d
@@ -0,0 +1,11 @@
+.\adctestmain.o: ADCTestMain.c
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
+.\adctestmain.o: ADCSWTrigger.h
+.\adctestmain.o: tm4c123gh6pm.h
+.\adctestmain.o: ST7735.h
+.\adctestmain.o: PLL.h
+.\adctestmain.o: Timer1.h
+.\adctestmain.o: fixed.h
